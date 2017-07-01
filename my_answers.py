77c279f0d398967ec6de3357996d7afb9d920ee6
Aliaksei Litouka <litovko@gmail.com>
2017-06-30 17:35:37 -0700

    Submission 0630

--- my_answers.py
+++ my_answers.py
@@ -3,6 +3,8 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+import re
+import string
 import keras
 
 
@@ -12,6 +14,10 @@ def window_transform_series(series,window_size):
     # containers for input/output pairs
     X = []
     y = []
+
+    for i in range(len(series) - window_size):
+        X.append(series[i:i + window_size])
+        y.append(series[i + window_size])
 
     # reshape each 
     X = np.asarray(X)
@@ -23,15 +29,24 @@ def window_transform_series(series,window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    np.random.seed(0)
+
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+    model.add(Dense(1))
+
+    optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=0.0)
+
+    model.compile(loss='mean_squared_error', optimizer=optimizer)
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
 def clean_text(text):
-    # find all unique characters in the text
+    text = re.sub("[^" + re.escape(string.ascii_lowercase) + re.escape(" !,.:;?") + "]", " ", text)
+    text = text.replace('  ', ' ')
 
-
-    # remove as many non-english characters and character sequences as you can 
+    return text
 
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -39,7 +54,11 @@ def window_transform_text(text,window_size,step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
-    
 
+    i = 0
+    while i < len(text) - window_size:
+        inputs.append(text[i:i + window_size])
+        outputs.append(text[i + window_size])
+        i = i + step_size
     
     return inputs,outputs
